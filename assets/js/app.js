0b309f62299a98741a3abcf0f2925df7925a777a
tinashe <giyavha_tinashe@berkeley.edu>
2019-12-03 23:19:40 -0800

    Adds database logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,3 +21,128 @@ var firebaseConfig = {
 
 var database = firebase.database();
 
+// Attach a listener to the database /players/ node to listen for any changes
+database.ref("/players/").on("value", function(snapshot) {
+	// Check for existence of player 1 in the database
+	if (snapshot.child("player1").exists()) {
+		console.log("Player 1 exists");
+
+		// Record player1 data and display
+		player1 = snapshot.val().player1;
+		player1Name = player1.name;
+		$("#playerOneName").text(player1Name);
+		$("#player1Stats").html("Win: " + player1.win + ", Loss: " + player1.loss + ", Tie: " + player1.tie);
+	} else {
+		console.log("Player 1 does NOT exist");
+
+		player1 = null;
+		player1Name = "";
+
+		// Update player1 display
+		$("#playerOneName").text("Waiting for Player 1...");
+		$("#playerPanel1").removeClass("playerPanelTurn");
+		$("#playerPanel2").removeClass("playerPanelTurn");
+		database.ref("/outcome/").remove();
+		$("#roundOutcome").html("Rock-Paper-Scissors");
+		$("#waitingNotice").html("");
+		$("#player1Stats").html("Win: 0, Loss: 0, Tie: 0");
+	}
+
+	// Check for existence of player 2 in the database
+	if (snapshot.child("player2").exists()) {
+		console.log("Player 2 exists");
+		player2 = snapshot.val().player2;
+		player2Name = player2.name;
+		$("#playerTwoName").text(player2Name);
+		$("#player2Stats").html("Win: " + player2.win + ", Loss: " + player2.loss + ", Tie: " + player2.tie);
+	} else {
+		console.log("Player 2 does NOT exist");
+
+		player2 = null;
+		player2Name = "";
+
+		// Update player2 display
+		$("#playerTwoName").text("Waiting for Player 2...");
+		$("#playerPanel1").removeClass("playerPanelTurn");
+		$("#playerPanel2").removeClass("playerPanelTurn");
+		database.ref("/outcome/").remove();
+		$("#roundOutcome").html("Rock-Paper-Scissors");
+		$("#waitingNotice").html("");
+		$("#player2Stats").html("Win: 0, Loss: 0, Tie: 0");
+	}
+
+    //
+	if (player1 && player2) {
+	
+		$("#playerPanel1").addClass("playerPanelTurn");
+		$("#waitingNotice").html(`Waiting on ${player1Name} to play`);
+	}
+
+	if (!player1 && !player2) {
+		database.ref("/chat/").remove();
+		database.ref("/turn/").remove();
+		database.ref("/outcome/").remove();
+
+		$("#chatDisplay").empty();
+		$("#playerPanel1").removeClass("playerPanelTurn");
+		$("#playerPanel2").removeClass("playerPanelTurn");
+		$("#roundOutcome").html("Rock-Paper-Scissors");
+		$("#waitingNotice").html("");
+	}
+});
+
+
+database.ref("/players/").on("child_removed", function(snapshot) {
+	var msg = snapshot.val().name + " has disconnected!";
+	var chatKey = database.ref().child("/chat/").push().key;
+
+	database.ref("/chat/" + chatKey).set(msg);
+});
+
+
+database.ref("/chat/").on("child_added", function(snapshot) {
+	var chatMsg = snapshot.val();
+	var chatEntry = $("<div>").html(chatMsg);
+
+	if (chatMsg.includes("disconnected")) {
+		chatEntry.addClass("chatColorDisconnected");
+	} else if (chatMsg.includes("joined")) {
+		chatEntry.addClass("chatColorJoined");
+	} else if (chatMsg.startsWith(yourPlayerName)) {
+		chatEntry.addClass("chatColor1");
+	} else {
+		chatEntry.addClass("chatColor2");
+	}
+
+	$("#chatDisplay").append(chatEntry);
+	$("#chatDisplay").scrollTop($("#chatDisplay")[0].scrollHeight);
+});
+
+database.ref("/turn/").on("value", function(snapshot) {
+
+	if (snapshot.val() === 1) {
+		console.log("TURN 1");
+		turn = 1;
+
+		if (player1 && player2) {
+			$("#playerPanel1").addClass("playerPanelTurn");
+			$("#playerPanel2").removeClass("playerPanelTurn");
+			$("#waitingNotice").html(`Waiting on ${player1Name} to play`);
+		}
+	} else if (snapshot.val() === 2) {
+		console.log("TURN 2");
+		turn = 2;
+
+		if (player1 && player2) {
+			$("#playerPanel1").removeClass("playerPanelTurn");
+			$("#playerPanel2").addClass("playerPanelTurn");
+			$("#waitingNotice").html(`Waiting on ${player2Name} to play`);
+		}
+	}
+});
+
+
+database.ref("/outcome/").on("value", function(snapshot) {
+	$("#roundOutcome").html(snapshot.val());
+});
+
