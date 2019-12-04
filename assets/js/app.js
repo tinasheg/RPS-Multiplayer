4730f94e2707d5b6f5647a4be0793cd10a8a3dd9
tinashe <giyavha_tinashe@berkeley.edu>
2019-12-03 23:25:18 -0800

    Adds game logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -146,3 +146,176 @@ database.ref("/outcome/").on("value", function(snapshot) {
 	$("#roundOutcome").html(snapshot.val());
 });
 
+
+
+$("#add-name").on("click", function(event) {
+	event.preventDefault();
+
+	if ( ($("#name-input").val().trim() !== "") && !(player1 && player2) ) {
+
+		if (player1 === null) {
+			console.log("Adding Player 1");
+
+			yourPlayerName = $("#name-input").val().trim();
+			player1 = {
+				name: yourPlayerName,
+				win: 0,
+				loss: 0,
+				tie: 0,
+				choice: ""
+			};
+
+			database.ref().child("/players/player1").set(player1);
+			database.ref().child("/turn").set(1);
+
+			database.ref("/players/player1").onDisconnect().remove();
+		} else if( (player1 !== null) && (player2 === null) ) {
+			console.log("Adding Player 2");
+
+			yourPlayerName = $("#name-input").val().trim();
+			player2 = {
+				name: yourPlayerName,
+				win: 0,
+				loss: 0,
+				tie: 0,
+				choice: ""
+			};
+
+			database.ref().child("/players/player2").set(player2);
+			database.ref("/players/player2").onDisconnect().remove();
+		}
+		var msg = yourPlayerName + " has joined!";
+		console.log(msg);
+		var chatKey = database.ref().child("/chat/").push().key;
+
+		database.ref("/chat/" + chatKey).set(msg);
+		$("#name-input").val("");	
+	}
+});
+
+$("#chat-send").on("click", function(event) {
+	event.preventDefault();
+
+	if ( (yourPlayerName !== "") && ($("#chat-input").val().trim() !== "") ) {
+		
+		var msg = yourPlayerName + ": " + $("#chat-input").val().trim();
+		$("#chat-input").val("");
+
+		
+		var chatKey = database.ref().child("/chat/").push().key;
+		database.ref("/chat/" + chatKey).set(msg);
+	}
+});
+
+
+$("#playerPanel1").on("click", ".panelOption", function(event) {
+	event.preventDefault();
+
+
+	if (player1 && player2 && (yourPlayerName === player1.name) && (turn === 1) ) {
+	
+		var choice = $(this).text().trim();
+
+		player1Choice = choice;
+		database.ref().child("/players/player1/choice").set(choice);
+
+		turn = 2;
+		database.ref().child("/turn").set(2);
+	}
+});
+
+$("#playerPanel2").on("click", ".panelOption", function(event) {
+	event.preventDefault();
+
+	
+	if (player1 && player2 && (yourPlayerName === player2.name) && (turn === 2) ) {
+
+		var choice = $(this).text().trim();
+
+		player2Choice = choice;
+		database.ref().child("/players/player2/choice").set(choice);
+
+		rpsCompare();
+	}
+});
+
+// Game logic
+function rpsCompare() {
+	if (player1.choice === "Rock") {
+		if (player2.choice === "Rock") {
+			// Tie
+			console.log("tie");
+
+			database.ref().child("/outcome/").set("Tie game!");
+			database.ref().child("/players/player1/tie").set(player1.tie + 1);
+			database.ref().child("/players/player2/tie").set(player2.tie + 1);
+		} else if (player2.choice === "Paper") {
+			// Player2 wins
+			console.log("paper wins");
+
+			database.ref().child("/outcome/").set("Paper wins!");
+			database.ref().child("/players/player1/loss").set(player1.loss + 1);
+			database.ref().child("/players/player2/win").set(player2.win + 1);
+		} else { // scissors
+			// Player1 wins
+			console.log("rock wins");
+
+			database.ref().child("/outcome/").set("Rock wins!");
+			database.ref().child("/players/player1/win").set(player1.win + 1);
+			database.ref().child("/players/player2/loss").set(player2.loss + 1);
+		}
+
+	} else if (player1.choice === "Paper") {
+		if (player2.choice === "Rock") {
+			// Player1 wins
+			console.log("paper wins");
+
+			database.ref().child("/outcome/").set("Paper wins!");
+			database.ref().child("/players/player1/win").set(player1.win + 1);
+			database.ref().child("/players/player2/loss").set(player2.loss + 1);
+		} else if (player2.choice === "Paper") {
+			// Tie
+			console.log("tie");
+
+			database.ref().child("/outcome/").set("Tie game!");
+			database.ref().child("/players/player1/tie").set(player1.tie + 1);
+			database.ref().child("/players/player2/tie").set(player2.tie + 1);
+		} else { // Scissors
+			// Player2 wins
+			console.log("scissors win");
+
+			database.ref().child("/outcome/").set("Scissors win!");
+			database.ref().child("/players/player1/loss").set(player1.loss + 1);
+			database.ref().child("/players/player2/win").set(player2.win + 1);
+		}
+
+	} else if (player1.choice === "Scissors") {
+		if (player2.choice === "Rock") {
+			// Player2 wins
+			console.log("rock wins");
+
+			database.ref().child("/outcome/").set("Rock wins!");
+			database.ref().child("/players/player1/loss").set(player1.loss + 1);
+			database.ref().child("/players/player2/win").set(player2.win + 1);
+		} else if (player2.choice === "Paper") {
+			// Player1 wins
+			console.log("scissors win");
+
+			database.ref().child("/outcome/").set("Scissors win!");
+			database.ref().child("/players/player1/win").set(player1.win + 1);
+			database.ref().child("/players/player2/loss").set(player2.loss + 1);
+		} else {
+			// Tie
+			console.log("tie");
+
+			database.ref().child("/outcome/").set("Tie game!");
+			database.ref().child("/players/player1/tie").set(player1.tie + 1);
+			database.ref().child("/players/player2/tie").set(player2.tie + 1);
+		}
+
+	}
+
+	// Set the turn value to 1, as it is now player1's turn
+	turn = 1;
+    database.ref().child("/turn").set(1);
+}
